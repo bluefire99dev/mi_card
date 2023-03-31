7e881839464c958884d47c6be0f9b4ed2fc59ac6
bluefire99dev <bluefire99dev@gmail.com>
2023-03-31 20:06:11 +0530

    1st commit

---
+++ lib/main.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              CircleAvatar(
+                radius: 50.0,
+                backgroundColor: Colors.amber,
+                foregroundImage: AssetImage('images/download - Copy.jpeg'),
+              ),
+              Text(
+                "YOU ARE MAC",
+                style: TextStyle(
+                    fontSize: 40.0,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Schyler'),
+              ),
+              Text(
+                "DESIGHNER",
+                style: TextStyle(
+                    fontSize: 20.0,
+                    letterSpacing: 2.5,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 250.0,
+                child: Divider(
+                  color: Colors.white,
+                  thickness: 2.0,
+                ),
+              ),
+              // 1
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                // padding: EdgeInsets.all(10.0),
+                child: Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone_android,
+                      size: 30.0,
+                      color: Colors.amber,
+                    ),
+                    title: Text(
+                      '8980787735',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ),
+                ),
+              ),
+
+              // 2
+
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                // padding: EdgeInsets.all(10.0),
+                child: Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      size: 30.0,
+                      color: Colors.amber,
+                    ),
+                    title: Text(
+                      'rananeel1233@gamail.com',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
